9fab5377cb994d356ac30ab82675ae308fd707e4
hein_thant <hthant00chk@gmail.com>
2022-12-21 11:30:19 +0630

    Clean data from leaderboard

--- src/modules/table-manager.js
+++ src/modules/table-manager.js
@@ -2,22 +2,39 @@ import { getLeaderboard } from './api-helper.js';
 
 const makeRowItem = (data) => {
   const li = document.createElement('li');
-  li.classList.add('score-item');
   const username = document.createElement('p');
+  const score = document.createElement('p');
   username.classList.add('username');
   username.innerText = data.user;
-  const score = document.createElement('p');
   score.classList.add('score');
   score.innerText = data.score;
+  li.classList.add('score-item');
   li.appendChild(username);
   li.appendChild(score);
   return li;
 };
 
+const cleanData = (leaderboard) => {
+  // If duplicate users, keep the one with highest score
+  const copyData = leaderboard.slice();
+  copyData.forEach((item) => {
+    const duplicates = leaderboard.filter(
+      (a) => a.user.toLowerCase() === item.user.toLowerCase()
+    );
+    duplicates.sort((a, b) => b.score - a.score);
+    duplicates.forEach((data, index) => {
+      if (index > 0) {
+        leaderboard.splice(leaderboard.indexOf(data), 1);
+      }
+    });
+  });
+  leaderboard.sort((a, b) => b.score - a.score);
+};
+
 const refreshTable = async () => {
   const scoreContainer = document.querySelector('#score-container');
   const leaderboard = await getLeaderboard();
-
+  cleanData(leaderboard);
   scoreContainer.textContent = '';
   leaderboard.forEach((data) => {
     const row = makeRowItem(data);
